c322e33e7c8f8f3ac1c84b5eed6310e066d1e6b6
srdjan-bozovic <srdjan.bozovic@gmail.com>
2015-11-26 10:44:25 +0100

    fix

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var button = document.getElementById('clickme');
-button.onclick = function(){
-	var msg = document.getElementById('message');
-	msg.innerText = 'Hello World!';
+window.onload = function(){
+	var button = document.getElementById('clickme');
+	button.onclick = function(){
+		var msg = document.getElementById('message');
+		msg.innerText = 'Hello World!';
+	}
 }
